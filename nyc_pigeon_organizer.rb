12696962d905fff65348b0a090223df59e3a8dcd
Francis <fmendez884@gmail.com>
2019-05-14 22:33:33 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+require 'byebug'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  data.each do |category, stats|
+    stats.each do |attribute, pigeon_array|
+      pigeon_array.each do |pigeon|
+        if !pigeon_list[pigeon]
+          pigeon_list[pigeon] = {}
+        end
+        if !pigeon_list[pigeon][category]
+          pigeon_list[pigeon][category] = []
+        end
+        pigeon_list[pigeon][category] << attribute.to_s
+      end
+    end
+  end
+  return pigeon_list
 end
