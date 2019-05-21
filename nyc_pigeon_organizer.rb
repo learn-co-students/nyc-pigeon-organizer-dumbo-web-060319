e72e66ce12b49de9a2e8a9a40c716e0f75e45b15
Garrett Halstein <garrett.halstein@emory.edu>
2019-05-21 19:26:16 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribute, info|
+  	info.each do |spec, names|
+  	  names.each do |name|
+  	    if !new_hash.include?(name)
+  	      new_hash[name] = {attribute => [spec.to_s]}
+  	    elsif !new_hash[name].include?(attribute)
+          new_hash[name][attribute] = [spec.to_s]
+        else
+  	  	  new_hash[name][attribute].push(spec.to_s)
+  	    end
+  	  end
+    end
+  end
+  new_hash
 end
