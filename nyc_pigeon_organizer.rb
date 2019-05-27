7c624192dd704aa61d9a0420df96be4f1e4c9a44
Alexander Schelchere <ascellie@gmail.com>
2019-05-26 21:52:03 -0400

    Accesss all the names in each category

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+
+  data.map do |category, attributes|
+    if category == :gender
+      attributes.map do |gender, names|
+        names.map {|name| puts name}
+      end
+    elsif category == :color
+      attributes.map do |color, names|
+        names.map {|name| puts name}
+      end
+    elsif category == :lives
+      attributes.map do |location, names|
+        names.map {|name| puts name}
+      end
+    end
+  end
+
 end
