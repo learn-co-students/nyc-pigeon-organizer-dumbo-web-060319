dd00033fbcb70916f3936cab9135134bdfe0b953
github username <github email address>
2019-05-27 00:14:14 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  puts "hello"
+ result={}
+ data.each do |k1,v1| #k1=:color v1=:purple => ["Theo", "Peter Jr.", "Lucky"],
+  v1.each do |k2,v2|  #k2=:purple v2=["Theo", "Peter Jr.", "Lucky"]
+    v2.each do |name|
+      result[name]={color:[],gender:[],lives:[]}
+    end
+  end
+ end
+ data.each do |k1,v1| #k1=:color v1=:purple => ["Theo", "Peter Jr.", "Lucky"],
+  v1.each do |k2,v2|  #k2=:purple v2=["Theo", "Peter Jr.", "Lucky"]
+    v2.each do |name|
+      result[name][k1]<< k2.to_s
+    end
+  end
 end
+result
+end
+
