6aab27fd3f1372660ca7422e5d0b1181b45a0f54
Alexander Schelchere <ascellie@gmail.com>
2019-05-27 10:04:04 -0400

    nyc pigeon organizer - complete

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,70 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  new_hash = {}
+  # Create hash to store organized pigeons
+  pigeons = {}
 
   data.map do |category, attributes|
-    if category == :gender
-      attributes.map do |gender, names|
-        names.map {|name| puts name}
-      end
-    elsif category == :color
+    if category == :color
       attributes.map do |color, names|
-        names.map {|name| puts name}
+        names.map do |name|
+          if !pigeons.has_key?(name)
+            pigeons[name] = {}
+          end
+          if !pigeons[name].has_key?(:color)
+            pigeons[name][:color] = []
+          end  
+          # binding.pry 
+          if data[category][color].include?(name)    
+            pigeons[name][:color].push(color.to_s)
+          end
+        end
+      end
+    elsif category == :gender
+      attributes.map do |gender, names|
+        names.map do |name|
+          if !pigeons[name].has_key?(:gender)
+            pigeons[name][:gender] = []
+          end
+          if data[category][gender].include?(name)    
+            pigeons[name][:gender].push(gender.to_s)
+          end
+        end
       end
     elsif category == :lives
       attributes.map do |location, names|
-        names.map {|name| puts name}
+        names.map do |name|
+          if !pigeons[name].has_key?(:lives)
+            pigeons[name][:lives] = []
+          end
+          if data[category][location].include?(name)    
+            pigeons[name][:lives].push(location.to_s)
+          end
+        end
       end
     end
   end
-
+  # binding.pry
+  pigeons
 end
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+nyc_pigeon_organizer(pigeon_data)
