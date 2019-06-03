c7934410716bfde51307ec791bfe4b69404ceb02
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-06-02 22:12:39 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
-def nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer (pigeons_data)
   # write your code here!
+  new_data = {}
+  #find data names for all sets
+  pigeons_data.each do |main_key, info|
+    names = info.values
+    #populate the keys
+    names.each do |each_group|
+      each_group.each do |each_name|
+        if !new_data.has_key?(each_name)
+          new_data[each_name] = {color: [], gender: [], lives: []}
+        end
+      end
+    end
+  end
+
+  #populate data
+  new_data.each do |name, data|
+    data.each do |attribute, valuearray|
+      pigeons_data[attribute].each do |value, namearray|
+        if namearray.include?(name)
+          new_data[name][attribute] << value.to_s
+        end
+      end
+    end
+  end
+  
 end
